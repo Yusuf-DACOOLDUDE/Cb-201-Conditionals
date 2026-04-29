cf3235ecd44fb460180ddf2cb1c6e2ff33ecb636
Yusuf-DACOOLDUDE <yusuf.stockman@codedbykids.org>
2026-04-29 22:16:17 +0000

    grades jawn

---
+++ app.js
@@ -0,0 +1,14 @@
+let grade=69
+
+if (grade >=90){
+    console.log("A")
+}
+else if (grade >=80){
+    console.log ("You gotta lock in you have a B")
+}
+else if (grade >=70){
+    console.log("D's are basically failing")
+}
+else{
+    console.log("...Bro... Why are your grades ever this low... Imma have to disown you if they stay like this for more than a week")
+}
